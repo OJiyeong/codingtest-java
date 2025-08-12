d5fa39eee6a0ef2451989e5811e7bde6e4d6c853
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2025-08-12 17:37:43 +0900

    [level 0] Title: 문자열 밀기, Time: 0.47 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        int count = -1;
+        StringBuilder sb1 = new StringBuilder(A);
+        StringBuilder sb2 = new StringBuilder(A);
+        int index = sb2.length();
+        
+        if(A.equals(B)) {
+            answer = 0;
+        } else {
+            for(int i = 0; i < index; i++) {
+                sb1.setCharAt(0, sb2.charAt(index-1));
+                for(int j = 1; j < sb2.length(); j++) {
+                    sb1.setCharAt(j, sb2.charAt(j-1));
+                }
+                
+                sb2.setLength(0);
+                sb2.append(sb1);
+                
+
+                if(sb1.toString().equals(B)) {
+                    count = i+1;
+                    break;
+                }
+            }
+            
+            answer = count;
+        
+        }
+        return answer;
+    }
+}
