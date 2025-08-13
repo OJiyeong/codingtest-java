bc44d5558c004eb8b4e94666f932d34277345021
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2025-08-13 17:33:25 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -7,19 +7,18 @@ class Solution {
         for(int i = 0; i < my_string.length(); i++) {
             char c = my_string.charAt(i);
             if(c >= 48 && c <= 57) {
-                // int n = Integer.parseInt(String.valueOf(c));
                 int n = c - '0';
                 list.add(n);
             }
         }
-        // list.sort();
+        
+        list.sort(null);
         
         int answer[] = new int[list.size()];
         
         for(int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
-        Arrays.sort(answer);
      
         return answer;
     }
