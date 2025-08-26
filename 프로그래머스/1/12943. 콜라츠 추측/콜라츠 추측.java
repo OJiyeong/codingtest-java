163575417f6077b1b7dfce01c47c60c91f7ef8d8
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2025-08-26 09:42:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,13 +3,8 @@ class Solution {
         long n = num;
         int answer = 0;
         while(n != 1) {
-            if(n % 2 == 0){
-                n /= 2;
-                answer++;
-            } else {
-                n = n * 3 + 1;
-                answer++;
-            }
+            n = (n%2 == 0) ? n/2 : n*3+1;
+            answer++;
             
             if(answer == 500) return -1;
         }
