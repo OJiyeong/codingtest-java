024d03af74b0bd81859ebc836ea01752a9a51d7f
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2025-09-19 17:30:58 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         int idx = number.length;
         
-        for(int i = 0; i < idx; i++) {
-            for(int j = i + 1; j < idx; j++) {
+        for(int i = 0; i < idx - 2; i++) {
+            for(int j = i + 1; j < idx - 1; j++) {
                 for(int k = j + 1; k < idx; k++) {
                     int sum = number[i] + number[j] + number[k];
                     if(sum == 0) answer++;
